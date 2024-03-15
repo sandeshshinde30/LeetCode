957a56365d89f0188479489ec3d84ffb3fbd972d
Sandesh Shinde <sandeshshinde7047@gmail.com>
2024-03-15 12:27:44 +0530

    Product of Array Except Self - Medium

--- ques_238.cpp
+++ ques_238.cpp
@@ -5,25 +5,25 @@ public :
 vector<int>productExceptSelf(vector<int>& nums)
 {
     int n = nums.size();
-    vector<int> prefix(n),suffix(n),res(n);
-
-    prefix[0] = 1;
+    vector<int> prefix(n,1);
+    vector<int> suffix(n,1);
+    vector<int> ans(n,1);
+    
     for(int i=1;i<n;i++)
     {
-        prefix[i] = prefix[i-1] * nums[i-1];   
+        prefix[i] = nums[i-1] * prefix[i-1];
     }
 
-    suffix[n-1] = 1;
     for(int j=n-2;j>=0;j--)
     {
-        suffix[j] = suffix[j+1] * nums[j+1];   
+        suffix[j] = nums[j+1] * suffix[j+1];
     }
 
-    for(int k=0;k<n;k++)
+    for(int i=0;i<n;i++)
     {
-        res[k] = suffix[k] * prefix[k];
+       ans[i] = prefix[i]*suffix[i];     
     }
     
-    return res;   
+    return ans;   
 }
 };
